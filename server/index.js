6324504893f7b028e33d799f69bd27901257791c
MachoBoy <jiiwon0@gmail.com>
2022-08-31 21:34:00 -0700

    initialize search api route

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const axios = require('axios');
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+
+const searchRouter = require('./routes/searchResults');
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,16 +14,6 @@ const app = express();
 app.use(cors());
 
 //routes
-app.get('/api/search/:query', async (req, res) => {
-  try {
-    const query = req.params.query;
-    const results = await axios(
-      `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${process.env.API_KEY}`,
-    );
-    console.log(results.data);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+app.use('/api/search', searchRouter);
 
 app.listen(PORT, () => console.log(`The server is running on ${PORT}`));
